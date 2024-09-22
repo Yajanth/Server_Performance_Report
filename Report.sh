9b3a0ec3844fc4a27e0d74792a40820cb276d71f
Yajanth <yajanthrr@gmail.com>
2024-09-22 13:59:32 +0100

    generate_report

--- Report.sh
+++ Report.sh
@@ -16,8 +16,6 @@ kernel_version=$(uname -r)
 #get CPU info
 cpu_info=$(lscpu | grep "Model name:" | awk '{for (i=3; i<=NF; i++) printf $i " "; print ""}')
 
-
-
 #get Memory info
 total_mem=$(free -h | grep "Mem:" | awk '{print $2}')
 used_mem=$(free -h | grep "Mem:" | awk '{print $3}')
@@ -30,9 +28,11 @@ space_used=$(df -h| grep "/dev/nvme0n1p7" | awk '{print $3}')
 ip_address=$(ip -o -4 addr show | awk '{print $2 ":" $4}')
 
 #get server uptime
-up_time=$(uptime -p)
-echo $space_used
+up_time=$(uptime -p | awk '{printf $2 ":" $4 "hr" }') 
 
+#Generating the Report
+if [[ ! -f Report_aggr.csv ]]; then
+  echo "Date,Time,Hostname,Machine,Kernel_version,Total_Mem,Used_Mem,Free_Mem,Disk_space,IP_info,UpTime" > Report_aggr.csv 
+fi
 
-
-
+echo "$current_date,$current_time,$host_name,\"$cpu_info\",$kernel_version,$total_mem,$used_mem,$free_mem,$space_used,\"$ip_address\",$up_time" >> Report_aggr.csv
